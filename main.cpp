a5bebdcd7707fe3aba0b53a6eb5070de78957f18
Mateo Pomies <35891922+sauvajohnz@users.noreply.github.com>
2018-02-02 22:30:09 +0100

    Ajout du menu "help"

--- main.cpp
+++ main.cpp
@@ -18,34 +18,44 @@ int main()
             cout << "********************************************" << endl << endl << "Conversion CELSIUS - FAHRENHEIT/KELVIN" << endl << endl << "********************************************" << endl;
             cout << "---Menu---" << endl << "1 - <Celsius a Fahrenheit>" << endl << "2 - <Fahrenheit a Celsius>" <<endl;
             cout << endl << "3 - <Celsius a Kelvin> " << endl << "4 - <Kelvin a Celsius> " << endl << "----------" << endl;
+            cout << "tapez \"help\" si vous avez besoin d'aide" << endl << endl;
             cin >> choixMenu;
-            if (choixMenu=="1" || choixMenu=="2" || choixMenu=="3" || choixMenu=="4")
+            if (choixMenu=="1" || choixMenu=="2" || choixMenu=="3" || choixMenu=="4" ||choixMenu=="help")
                 continuerChoixMenu = false;
 
         }while (continuerChoixMenu);
 
+        if (choixMenu!="help")
+        {
         cout << "Choisissez la temperature a convertir" << endl;
         cin >> temperatureAConvertir;
+        }
 
         if(choixMenu=="1")
         {
             temperatureConvertie = temperatureAConvertir * 1.8 + 32;
-            cout << temperatureAConvertir << "Celsius = " << temperatureConvertie << "Fahrenheit" << endl;
+            cout << endl << temperatureAConvertir << "Celsius = " << temperatureConvertie << "Fahrenheit" << endl;
         }
         else if (choixMenu=="2")
         {
             temperatureConvertie = (temperatureAConvertir - 32) / 1.8;
-            cout << temperatureAConvertir << "Fahrenheit = " << temperatureConvertie << "Celsius" << endl;
+            cout << endl << temperatureAConvertir << "Fahrenheit = " << temperatureConvertie << "Celsius" << endl;
         }
         else if (choixMenu=="3")
         {
             temperatureConvertie = temperatureAConvertir + 273.15;
-            cout << temperatureAConvertir << "Celsius = " << temperatureConvertie << "Kelvin" << endl;
+            cout << endl << temperatureAConvertir << "Celsius = " << temperatureConvertie << "Kelvin" << endl;
         }
         else if (choixMenu=="4")
         {
             temperatureConvertie = temperatureAConvertir - 273.15;
-            cout << temperatureAConvertir << "Kelvin = " << temperatureConvertie << "Celsius" << endl;
+            cout << endl << temperatureAConvertir << "Kelvin = " << temperatureConvertie << "Celsius" << endl;
+        }
+        else if (choixMenu=="help")
+        {
+            system("cls");
+            cout << "--" << endl << "MENU AIDE" << endl << "--" << endl << "Pour choisir une unite a convertir, tapez le numero associe."<< endl << endl << "Exemple :" << endl;
+            cout << "Si vous voulez convertir 30 Degres Celsius en Fahrenheit tapez \"1\" puis mettez le nombre \"30\"" << endl << endl;
         }
         do{
             cout << "Voulez vous continuer ? (O/N)" << endl;
